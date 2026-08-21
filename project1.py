e6d5b0d72f29c78db32aacafe298346dba2342a2
mariyamkholafa09 <mkholafa309@gmail.com>
2026-08-21 20:11:45 +0300

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,11 @@
+print ('welcome')
+name= input('Enter your name : ',)
+print("Hello", name)
+
+print ('Would you solve this equation')
+print ('5**2')
+answer = int(input())
+if answer == 25 :
+    print('You are advanced to stage2 ')
+else:
+    print('Wrong answer')
