923074e6a62b1029da9682cb12ad58476c07fcd3
Lamaamir <lamaamir491@gmail.com>
2026-08-21 20:49:16 +0300

    New file

--- project1.py
+++ project1.py
@@ -7,5 +7,11 @@ print ('5**2')
 answer = int(input())
 if answer == 25 :
     print('You are advanced to stage2 ')
+    print("I have hands but I can't clap, I have a face but I can't smile, What am I ?")
+    answer2 = input()
+    if answer2 == "clock":
+        print("correct")
+    else:
+        print("wrong")
 else:
     print('Wrong answer')
